30a5d5cb251e07d587b0395268e3990ead57a0d7
Mateus <mateus@outlook.com.br>
2025-05-11 11:33:21 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/br/com/univesp/pi/scorpions/ScorpionsApplication.java
@@ -0,0 +1,13 @@
+package br.com.univesp.pi.scorpions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScorpionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScorpionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/univesp/pi/scorpions/ScorpionsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.univesp.pi.scorpions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScorpionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
