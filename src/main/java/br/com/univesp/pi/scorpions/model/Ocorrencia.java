164dbf648b64b565df129758699b9c16d4b65d6f
Mateus <mateus@outlook.com.br>
2025-05-12 20:25:10 -0300

    Criãção da classe Ocorrencia

---
+++ src/main/java/br/com/univesp/pi/scorpions/model/Ocorrencia.java
@@ -0,0 +1,124 @@
+package br.com.univesp.pi.scorpions.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Ocorrencia {
+
+    private int id;
+
+    private String nome;
+
+    private String email;
+
+    private String telefone;
+
+    private double latitude;
+
+    private double longitude;
+
+    private String bairro;
+
+    private String cidade;
+
+    private LocalDateTime dataOcorrencia;
+
+    public Ocorrencia(int id, String nome, String email, String telefone, double latitude, double longitude, String bairro, String cidade, LocalDateTime dataOcorrencia) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.dataOcorrencia = dataOcorrencia;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public LocalDateTime getDataOcorrencia() {
+        return dataOcorrencia;
+    }
+
+    public void setDataOcorrencia(LocalDateTime dataOcorrencia) {
+        this.dataOcorrencia = dataOcorrencia;
+    }
+
+    @Override
+    public String toString() {
+        return "Ocorrencia{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                ", bairro='" + bairro + '\'' +
+                ", cidade='" + cidade + '\'' +
+                ", dataOcorrencia=" + dataOcorrencia +
+                '}';
+    }
+}
